ebcf211829e5387ed4c2887f6d80eb61e21566b4
Viraj Solanki <virazssolanki@gmail.com>
2019-04-22 15:04:50 +0530

    db added, form add, flash add

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+from app import app, db
+from app.models import User
 
 
-@app.route('/')
-@app.route('/index')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Subscribe')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = User(email=form.email.data)
+        db.session.add(email)
+        db.session.commit()
+        flash('Subscribed')
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
